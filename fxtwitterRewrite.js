3cf076544edb60c1b5a76a154dfeeb6211e13cc7
Quinn Kleinfelter <qkleinfelter@qkleinfelter.com>
2023-10-25 19:51:45 -0400

    initial commit

---
+++ fxtwitterRewrite.js
@@ -0,0 +1,49 @@
+setInterval(() => {
+  const linkElement = findElementByText("Copy link");
+  if (linkElement) {
+    linkElement.addEventListener("click", () => {
+      setTimeout(() => {
+        readFromClipboard(writeNewLinkToClipboard);
+      }, 500);
+    });
+  }
+}, 100);
+
+function findElementByText(text) {
+  const elements = document.querySelectorAll("*");
+  for (let element of elements) {
+    if (element.textContent === text) {
+      return element;
+    }
+  }
+  return null;
+}
+
+function readFromClipboard(callback) {
+  navigator.clipboard
+    .readText()
+    .then((clipText) => {
+      callback(clipText);
+    })
+    .catch((error) => {
+      console.error("Unable to read from clipboard");
+    });
+}
+
+function writeToClipboard(text) {
+  navigator.clipboard.writeText(text).then(
+    () => {
+      console.log("Text successfully copied to clipboard: ", text);
+    },
+    () => {
+      console.error("Unable to write to clipboard");
+    }
+  );
+}
+
+function writeNewLinkToClipboard(text) {
+  let newText = text
+    .replace("twitter.com", "fxtwitter.com")
+    .replace("x.com", "fixupx.com");
+  writeToClipboard(newText);
+}
